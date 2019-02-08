9dc73c8946a0503670765d854dd06409b00b519f
Al Warren <>
2019-02-08 07:35:13 -0600

    Modified License

--- app/src/main/java/com/ntxdroid/filterdemo/feature/mission/MissionsViewModel.kt
+++ app/src/main/java/com/ntxdroid/filterdemo/feature/mission/MissionsViewModel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019.
+ * Copyright (c) 2019. Al Warren
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
